b2208ecda92f090875000c661f2adbe2ed4fba00
Ubuntu <ubuntu@ip-172-31-15-220.us-east-2.compute.internal>
2023-03-15 03:50:47 +0000

    Fixed return codes for error conditions

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -11,6 +11,7 @@ int main(int argc, char* argv[]) {
 	if(argc !=3) {
 		printf("Usage : %s <directory\\file> <string>\n",argv[0]);
 		syslog(LOG_ERR,"Invalid number of arguments: %d",argc);
+		return 1;
 	}
 
 	syslog(LOG_INFO,"Writing %s to %s",argv[2],argv[1]);
@@ -23,6 +24,7 @@ int main(int argc, char* argv[]) {
 	} else {
 		printf("Error creating file %s: %s\n",argv[2],strerror(errno));
 		syslog(LOG_ERR,"Error creating file %s: %s",argv[2],strerror(errno));
+		return 1;
 	}
 
 	closelog();
